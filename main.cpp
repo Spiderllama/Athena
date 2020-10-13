5186df4b5d9d4f904aaf6a0c1cf0669334f91642
Spiderllama <72757628+Spiderllama@users.noreply.github.com>
2020-10-13 08:09:15 -0400

    Changed functions to string. Halfway through add.hero update.

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@
 int main() {
 	Heroes Ana;
 	PlaySound(TEXT("Overwatch.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
-	splash_screen(false);
+	std::cout << splash_screen();
 	std::cin.ignore();
 	Ana.add_hero();
-	Ana.hero_name("Winston");
+	
 	system("cls");
-	Ana.hero_info();
+	std::cout << Ana.hero_info();
 	
 }
